983972281fc47d5eed4ec6b087893c6ba6bd20f3
puyul123 <110555492+puyul123@users.noreply.github.com>
2022-11-16 15:27:53 +0700

    Deleting some images

--- src/Util/LoadSave.java
+++ src/Util/LoadSave.java
@@ -12,7 +12,7 @@ import Main.Game;
 
 public class LoadSave {
 	
-	public static final String PLAYER_ATLAS = "player_sprites.png";
+	
 	public static final String LEVEL_ATLAS = "test_outside_sprites.png";
 	public static final String LEVEL_ONE_DATA = "test_level_one_data.png";
 	public static final String LEVEL_ONE_DATA_LONG = "level_one_data_long.png";
