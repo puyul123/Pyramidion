b5bbbeeb53bcf53747952a5294cbf1e306dec33c
puyul123 <110555492+puyul123@users.noreply.github.com>
2022-11-30 14:41:51 +0700

    attackbox mummy

--- src/Entity/Mummy.java
+++ src/Entity/Mummy.java
@@ -10,27 +10,48 @@ import static Util.HelpMethods.GetEntityYPosUnderRoofOrAboveFloor;
 import static Util.HelpMethods.IsEntityOnFloor;
 import static Util.HelpMethods.IsFloor;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
 
 import Main.Game;
 
 public class Mummy extends Enemy{
-
+	
+	private Rectangle2D.Float attackBox;
+	private int attackBoxOffsetX;
+	
 	public Mummy(float x, float y) {
 		super(x, y, (int)(MUMMY_SIZE * Game.SCALE), (int)(MUMMY_SIZE * Game.SCALE), MUMMY);
 		initCollision(x, y, 30.7f * Game.SCALE, 42 * Game.SCALE);
-		
+		initAttackBox();
 	}
 	
+	
+	private void initAttackBox() {
+		attackBox = new Rectangle2D.Float(x, y, (int) (55 * Game.SCALE), (int)(42 * Game.SCALE));
+		attackBoxOffsetX = (int) (Game.SCALE * 30);
+	}
+
+
 	public void draw(Graphics g, int xLvlOffset) {
 		drawCollision(g, xLvlOffset);
+		g.setColor(Color.RED);
+		g.drawRect((int)(attackBox.x - xLvlOffset), (int)attackBox.y, (int)attackBox.width, (int)attackBox.height);
 	}
 	
 	public void update(int[][] lvlData, Player player) {
 		updateAnimationTick();
 		updateMove(lvlData, player);
+		updateAttackBox();
 	}
 	
+	private void updateAttackBox() {
+		attackBox.x = collision.x - attackBoxOffsetX + 25;
+		attackBox.y = collision.y;		
+	}
+
+
 	private void updateMove(int[][] lvlData, Player player) {
 		if(firstUpdate) 
 			firstUpdateCheck(lvlData);
@@ -44,7 +65,6 @@ public class Mummy extends Enemy{
 				break;
 			case RUNNING:
 				if (canSeePlayer(lvlData, player)) {
-					System.out.println("TURN");
 					turnTowardsPlayer(player);
 				}
 				if (isPlayerCloseForAttack(player))
